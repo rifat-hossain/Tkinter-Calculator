c50e888bc6266f1df45dab965b70cbad779bfd90
Rifat Hossain <rifathossainayon@gmail.com>
2024-10-05 21:20:47 +0600

    Key binding + bug fixes

--- calculator.py
+++ calculator.py
@@ -4,50 +4,84 @@ import math
 
 r = tk.Tk()
 r.title('Calculator')
-disp = tk.Frame(r)
 
-res_disp = tk.Label(disp,text='0',font=('Arial',25),anchor=tk.E,bg='white')
+###################################
+#       Creating Display
+###################################
+
+disp = tk.Frame(r)
+res_disp = tk.Label(disp,text='0',font=('Arial',25),anchor=tk.E,bg='white') # Result display
 res_disp.pack(fill=tk.X)
-eq_disp = tk.Label(disp,text='0',font=('Arial',11),anchor=tk.E,bg='white')
+eq_disp = tk.Label(disp,text='0',font=('Arial',11),anchor=tk.E,bg='white') # Equation display
 eq_disp.pack(fill=tk.X)
 disp.pack(fill=tk.X)
 
-kb1 = tk.Frame(r,bg='black')
+###################################
+#       Creating Buttons
+###################################
+
+kb1 = tk.Frame(r,bg='black')    #Row 1
 tk.Button(kb1,text='%',width=5,command=lambda: percent())     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb1,text='CE',width=5,command=lambda: rem_ce())    .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb1,text='C',width=5,command=lambda: rem_c())     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb1,text='⌫',width=5,command=lambda: backspace())     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 kb1.pack(fill=tk.BOTH,expand=True)
-kb2 = tk.Frame(r,bg='black')
+kb2 = tk.Frame(r,bg='black')    #Row 2
 tk.Button(kb2,text='1/x',width=5,command=lambda: execute('1/'))   .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb2,text='x²',width=5,command=lambda: execute('²'))    .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb2,text='√x',width=5,command=lambda: execute('√'))    .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb2,text='÷',width=5,command=lambda: set_operator('÷'))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 kb2.pack(fill=tk.BOTH,expand=True)
-kb3 = tk.Frame(r,bg='black')
+kb3 = tk.Frame(r,bg='black')    #Row 3
 tk.Button(kb3,text='7',width=5,command=lambda: num_pressed(7))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb3,text='8',width=5,command=lambda: num_pressed(8))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb3,text='9',width=5,command=lambda: num_pressed(9))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb3,text='×',width=5,command=lambda: set_operator('×'))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 kb3.pack(fill=tk.BOTH,expand=True)
-kb4 = tk.Frame(r,bg='black')
+kb4 = tk.Frame(r,bg='black')    #Row 4
 tk.Button(kb4,text='4',width=5,command=lambda: num_pressed(4))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb4,text='5',width=5,command=lambda: num_pressed(5))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb4,text='6',width=5,command=lambda: num_pressed(6))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb4,text='-',width=5,command=lambda: set_operator('-'))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 kb4.pack(fill=tk.BOTH,expand=True)
-kb5 = tk.Frame(r,bg='black')
+kb5 = tk.Frame(r,bg='black')    #Row 5
 tk.Button(kb5,text='1',width=5,command=lambda: num_pressed(1))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb5,text='2',width=5,command=lambda: num_pressed(2))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb5,text='3',width=5,command=lambda: num_pressed(3))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb5,text='+',width=5,command=lambda: set_operator('+'))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 kb5.pack(fill=tk.BOTH,expand=True)
-kb6 = tk.Frame(r,bg='black')
+kb6 = tk.Frame(r,bg='black')    #Row 6
 tk.Button(kb6,text='±',width=5,command=lambda: execute('±'))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb6,text='0',width=5,command=lambda: num_pressed(0))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb6,text='.',width=5,command=lambda: num_pressed('.'))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 tk.Button(kb6,text='=',width=5,command=lambda: execute('='))     .pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 kb6.pack(fill=tk.BOTH,expand=True)
+
+###################################
+#     Binding Keyboard Keys
+###################################
+
+r.bind("0",lambda x: num_pressed(0))
+r.bind("1",lambda x: num_pressed(1))
+r.bind("2",lambda x: num_pressed(2))
+r.bind("3",lambda x: num_pressed(3))
+r.bind("4",lambda x: num_pressed(4))
+r.bind("5",lambda x: num_pressed(5))
+r.bind("6",lambda x: num_pressed(6))
+r.bind("7",lambda x: num_pressed(7))
+r.bind("8",lambda x: num_pressed(8))
+r.bind("9",lambda x: num_pressed(9))
+r.bind(".",lambda x: num_pressed('.'))
+r.bind("+",lambda x: set_operator('+'))
+r.bind("-",lambda x: set_operator('-'))
+r.bind("*",lambda x: set_operator('×'))
+r.bind("/",lambda x: set_operator('÷'))
+r.bind("=",lambda x: execute('='))
+r.bind("<Return>",lambda x: execute('='))
+
+###################################
+#          Operations
+###################################
 
 def num_pressed(number):
     global l_op,op,r_op
@@ -58,7 +92,9 @@ def num_pressed(number):
     eq_disp['text'] = l_op + op + r_op
 
 def set_operator(operator):
-    global op,r_op
+    global l_op,op,r_op
+    if l_op == '':
+        l_op = '0'
     if r_op != '':
         execute('=')
     op = operator
@@ -136,10 +172,14 @@ def percent():
     if r_op != '':
         r_op = str(float(l_op)*float(r_op)/100)
         eq_disp['text'] = l_op + op + r_op
+        
+###################################
+#           Variables
+###################################
 
-l_op = ''
-r_op = ''
-op = ''
+l_op = ''       #Left Operand
+r_op = ''       #Right Operand
+op = ''         #Operator
 
 r.mainloop()
 
